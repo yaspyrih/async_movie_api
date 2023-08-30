467b64e890ee79ad5172599d9196444b2b03af43
yaspyrih <yaspyrih@gmail.com>
2023-08-30 11:45:17 -0300

    Movie API

---
+++ index.js
@@ -0,0 +1,40 @@
+const btnAnterior = document.getElementById(btnAnterior);
+const btnSiguiente = document.getElementById(btnSiguiente);
+let pagina = 1;
+
+const cargarPeliculas = async()=>{
+
+    try{
+
+        const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=191528030c357419329af1198edbcb24&language=es-MX&page=1`)
+        console.log(respuesta);
+
+        if(respuesta.status === 200){
+
+            const datos = await respuesta.json();
+
+            let peliculas = "";
+
+            datos.results.forEach(pelicula => {
+                peliculas += `
+                    <div class="peliculas">
+                        <img class="poster" src"https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                        <h3 class="titulo">${pelicula.title}</h3>
+                        <p>${pelicula.overview}</p>
+                    </div>
+                `;
+            });
+
+            document.getElementById("contenedor") .innerHTML = peliculas;
+        }
+
+        else if(respuesta.status === 401){console.log("key incorrecta");}
+        else if(respuesta.status === 404){console.log("no disponible");}
+        else if {console.log("no tengo idea del error");} 
+    }
+    catch(error){
+        console.log(error.message);
+    }
+}
+
+cargarPeliculas();
